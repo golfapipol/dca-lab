f925040d17d3997d1a97af4bd51ac98b829ca572
Apipol Sukgler <golf.apipol@gmail.com>
2021-10-17 20:41:57 +0700

    add og:tag and twitter:tag

--- site/pages/_app.js
+++ site/pages/_app.js
@@ -1,7 +1,17 @@
 import 'tailwindcss/tailwind.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (<>
+      <Head>
+        <title></title>
+        <meta property="og:title" content="ความมั่งคั่งออกแบบได้ WHAT-IF DCA Fund" key="title" />
+        <meta property="og:description" content="ถ้าเราลง DCA กองทุนไว้ในอดีตจะเป็นอย่างไร จะกำไร หรือ ขาดทุน?" key="description" />
+        <meta property="og:image" content="https://programmable-wealth-what-if-dca-fund.netlify.app/public/cover-1.png" key="image" />
+        <meta property="twitter:title" content="ความมั่งคั่งออกแบบได้ WHAT-IF DCA Fund" key="title" />
+        <meta property="twitter:description" content="ถ้าเราลง DCA กองทุนไว้ในอดีตจะเป็นอย่างไร จะกำไร หรือ ขาดทุน?" key="description" />
+        <meta property="twitter:image" content="https://programmable-wealth-what-if-dca-fund.netlify.app/public/cover-1.png" key="image" />
+      </Head>
       <script data-ad-client="ca-pub-4076028924879349" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       <Component {...pageProps} />
     </>)

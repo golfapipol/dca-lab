9e63e78aecb34468165752b234ea15d760fcd306
Apipol Sukgler <golf.apipol@gmail.com>
2021-10-17 21:00:00 +0700

    fix sharing tag

--- site/pages/_app.js
+++ site/pages/_app.js
@@ -5,15 +5,16 @@ function MyApp({ Component, pageProps }) {
   return (<>
       <Head>
         <title>ความมั่งคั่งออกแบบได้ WHAT-IF DCA Fund</title>
-        <meta property="og:type" content="article" key="title" />
+        <meta property="og:type" content="article" key="type" />
         <meta property="og:title" content="ความมั่งคั่งออกแบบได้ WHAT-IF DCA Fund" key="title" />
         <meta property="og:description" content="ถ้าเราลง DCA กองทุนไว้ในอดีตจะเป็นอย่างไร จะกำไร หรือ ขาดทุน?" key="description" />
-        <meta property="og:image" content="https://programmable-wealth-what-if-dca-fund.netlify.app/cover-1.png" key="image" />
+        <meta property="og:url" content="https://programmable-wealth-what-if-dca-fund.netlify.app"/>
+        <meta property="og:image" content="https://programmable-wealth-what-if-dca-fund.netlify.app/cover-1.png"/>
         <meta property="og:image:secure_url" content="https://programmable-wealth-what-if-dca-fund.netlify.app/cover-1.png" /> 
         <meta property="og:image:type" content="image/png" /> 
-        <meta property="twitter:title" content="ความมั่งคั่งออกแบบได้ WHAT-IF DCA Fund" key="title" />
-        <meta property="twitter:description" content="ถ้าเราลง DCA กองทุนไว้ในอดีตจะเป็นอย่างไร จะกำไร หรือ ขาดทุน?" key="description" />
-        <meta property="twitter:image" content="https://programmable-wealth-what-if-dca-fund.netlify.app/cover-1.png" key="image" />
+        <meta property="twitter:title" content="ความมั่งคั่งออกแบบได้ WHAT-IF DCA Fund" />
+        <meta property="twitter:description" content="ถ้าเราลง DCA กองทุนไว้ในอดีตจะเป็นอย่างไร จะกำไร หรือ ขาดทุน?" />
+        <meta property="twitter:image" content="https://programmable-wealth-what-if-dca-fund.netlify.app/cover-1.png"/>
       </Head>
       <script data-ad-client="ca-pub-4076028924879349" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       <Component {...pageProps} />

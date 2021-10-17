e62e75e0c211b0c0826db7e37b96387e9c6c2979
Apipol Sukgler <golf.apipol@gmail.com>
2021-10-17 20:54:29 +0700

    add og:image type and secure url

--- site/pages/_app.js
+++ site/pages/_app.js
@@ -8,6 +8,8 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:title" content="ความมั่งคั่งออกแบบได้ WHAT-IF DCA Fund" key="title" />
         <meta property="og:description" content="ถ้าเราลง DCA กองทุนไว้ในอดีตจะเป็นอย่างไร จะกำไร หรือ ขาดทุน?" key="description" />
         <meta property="og:image" content="https://programmable-wealth-what-if-dca-fund.netlify.app/cover-1.png" key="image" />
+        <meta property="og:image:secure_url" content="https://programmable-wealth-what-if-dca-fund.netlify.app/cover-1.png" /> 
+        <meta property="og:image:type" content="image/png" /> 
         <meta property="twitter:title" content="ความมั่งคั่งออกแบบได้ WHAT-IF DCA Fund" key="title" />
         <meta property="twitter:description" content="ถ้าเราลง DCA กองทุนไว้ในอดีตจะเป็นอย่างไร จะกำไร หรือ ขาดทุน?" key="description" />
         <meta property="twitter:image" content="https://programmable-wealth-what-if-dca-fund.netlify.app/cover-1.png" key="image" />

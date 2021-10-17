75f07c0fa3e2a1560aaa344584bc4b7cd550ebfb
Apipol Sukgler <golf.apipol@gmail.com>
2021-10-17 13:09:08 +0700

    add google adsense

--- site/pages/_app.js
+++ site/pages/_app.js
@@ -2,6 +2,7 @@ import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
   return (<>
+      <script data-ad-client="ca-pub-4076028924879349" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       <Component {...pageProps} />
     </>)
 }
